0b2301485984e662476ffdec4d8175d47c2af3bc
imamkun2001 <imamkun2708@gmail.com>
2021-09-13 20:50:48 +0700

    Menambahkan file contoh1 php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php 
+class contoh1 extends CI_Controller
+{
+    public function index()
+
+    {
+        echo "<h1> Perkenalan Diri <h1>";
+        echo "Hello, Perkenallkan nama saya Imam faturrahman Saya Tinggal di Jungkat dan Hobi saya adalah Bermain Bulu Tangkis, Browsing, Main Game, Belajar Membuat Website dan hal menarik lainnya yang menurut saya sangat menarik untuk dipelajari";
+    }
+}
+
+
+
+?>
