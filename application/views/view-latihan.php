6cd69ac5a248244ca601773aedae74f56bc95ddb
imamkun2001 <imamkun2708@gmail.com>
2021-09-20 13:56:11 +0700

    Menambahkan Latihan1, Model_Latihan1, View Latihan, htacess dan hilangkan index di config

---
+++ application/views/view-latihan.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latiihan 1</title>
+</head>
+
+<body>
+
+    Hallo Kawan.. Yuk Kita belajar web programing.>!!! <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
